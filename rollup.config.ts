703d0433a264ee20efb0d84898f03d9b5c6488f5
zhangjicheng <zhangjichengcc@163.com>
2023-02-02 19:01:52 +0800

    update 更新 create-app

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,25 +2,26 @@
  * @Author: zhangjicheng
  * @Date: 2022-07-11 11:42:43
  * @LastEditors: zhangjicheng
- * @LastEditTime: 2023-02-02 01:17:33
- * @FilePath: /create-app/rollup.config.ts
+ * @LastEditTime: 2023-02-02 18:56:17
+ * @FilePath: \create-app\rollup.config.ts
  */
 import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve"; // 帮助rollup查找外部模块
-import commonjs from "rollup-plugin-commonjs"; // 将commonjs转es6模块
+import { nodeResolve } from "@rollup/plugin-node-resolve"; // 帮助rollup查找外部模块
+import commonjs from "@rollup/plugin-commonjs"; // 将commonjs转es6模块
 import filesize from "rollup-plugin-filesize"; // 显示打包后包大小
-import { terser } from "rollup-plugin-terser"; // 压缩代码
-import { eslint } from "rollup-plugin-eslint";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
+import eslint from "@rollup/plugin-eslint";
+// import alias from "@rollup/plugin-alias";
 import ts from "rollup-plugin-typescript2";
-// import path from 'path';
 import packageJSON from "./package.json";
 
-const { TERSER } = process.env;
+// const { TERSER } = process.env;
 
 const plugins = [
-  resolve({
-    browser: true,
-  }),
+  ts(),
+  json(),
+  nodeResolve({ preferBuiltins: true }),
   commonjs({
     include: ["node_modules/**"],
   }),
@@ -28,27 +29,23 @@ const plugins = [
     exclude: "node_modules/**", // 只编译我们的源代码
   }),
   filesize(),
-  TERSER ? terser() : "",
+  terser(),
   eslint({
     throwOnError: true,
     include: ["src/**/*.ts"],
     exclude: ["node_modules/**", "lib/**"],
   }),
-  ts(),
 ];
 
 export default {
   input: "src/create-app.ts",
+
   output: [
     {
-      file: "create-app.cjs",
-      format: "cjs",
+      file: packageJSON.bin["app-cli"],
+      format: "es",
       exports: "auto",
-    },
-    {
-      file: "create-app.js",
-      format: "esm",
-      exports: "auto",
+      banner: "#!/usr/bin/env node",
     },
   ],
   plugins,
--- src/create-app.ts
+++ src/create-app.ts
@@ -1,3 +1,134 @@
-const a: string = "aaa";
+// #!/usr/bin/env node
 
-console.log(a);
+import { program } from "commander";
+import inquirer from "inquirer";
+import chalk from "chalk";
+// import fs from "fs";
+import ora from "ora";
+import logSymbols from "log-symbols";
+import download from "download-git-repo";
+import { spawnSync } from "child_process";
+import packageData from "../package.json";
+
+// const packageData = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+
+const templateList = {
+  Vue: "git@github.com:zhangjichengcc/create-app.git",
+  React: "git@github.com:zhangjichengcc/create-ap.git",
+  gitbook: "git@github.com:zhangjichengcc/create-app.git",
+  docsify: "git@github.com:zhangjichengcc/create-app.git",
+} as const;
+
+type TemplateKeys = keyof typeof templateList;
+
+/**
+ * @description: 下载项目模板
+ * @param {*} downLoadURL
+ * @param {*} target
+ * @return {*}
+ */
+function downloadProject(downLoadURL: string, target: string) {
+  const { error } = spawnSync("git", ["--version"]);
+  const githubUrl = downLoadURL.replace(
+    /^git@github\.com:(.*)\.git$/,
+    `github:$1#main`
+  );
+  if (error) {
+    console.log(
+      logSymbols.warning,
+      chalk.yellow("未添加Git环境变量引起，添加Git与git管理库的环境变量即可；")
+    );
+    console.log(
+      logSymbols.info,
+      chalk.green("或直接到模板地址下载：", downLoadURL)
+    );
+    return Promise.reject(error);
+  }
+  return new Promise<void>(function (resolve, reject) {
+    const spinner = ora("正在下载模板...");
+    spinner.start();
+    download(githubUrl, target, function (err: string) {
+      if (err) {
+        spinner.fail("模板下载失败");
+        if (err.toString().includes("status 128")) {
+          console.log(
+            "\n",
+            logSymbols.warning,
+            chalk.yellow(
+              "Git默认开启了SSL验证，请尝试执行下面命令，关闭验证后再重试；"
+            )
+          );
+          console.log(
+            logSymbols.info,
+            chalk.green("git config --global http.sslVerify false")
+          );
+        }
+        reject(err);
+      } else {
+        spinner.succeed("模板下载成功");
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * @description: 初始化项目模板
+ * @return {*}
+ */
+function initProject() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "projectName",
+        message: "请输入项目名称",
+      },
+      {
+        type: "list",
+        name: "frameTemplate",
+        message: "请选择框架类型",
+        choices: ["Vue", "React", "gitbook", "docsify"],
+      },
+    ])
+    .then((reason: { projectName: string; frameTemplate: TemplateKeys }) => {
+      const {
+        projectName,
+        frameTemplate,
+      }: {
+        projectName: string;
+        frameTemplate: TemplateKeys;
+      } = reason;
+      const downLoadURL = templateList[frameTemplate];
+      downloadProject(downLoadURL, projectName)
+        .then(() => {
+          console.log(logSymbols.success, chalk.green("项目初始化完成"));
+        })
+        .catch((err) => {
+          console.log(
+            "\n",
+            logSymbols.info,
+            chalk.yellow(`失败原因：${err}`),
+            "\n",
+            `请尝试直接下载模板：git clone ${downLoadURL}`
+          );
+        });
+    });
+}
+
+program
+  .version(packageData.version)
+  .command("init")
+  .description("create a new project")
+  .alias("i")
+  .action(() => {
+    initProject();
+  });
+
+program.parse(process.argv);
+
+// https://juejin.cn/post/6979511969736818701
+
+// https://juejin.cn/post/6911987404039520270#heading-11
+
+// https://blog.csdn.net/ZhaoQM_script/article/details/120631231
